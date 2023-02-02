2394414a6d325c64a639d5850d9ed515d52d3abf
francogallegos <francogallegos02@outlook.com>
2023-02-02 17:31:48 -0500

    carrusel semi finishes

--- src/components/home/Carrusel.vue
+++ src/components/home/Carrusel.vue
@@ -1,10 +1,9 @@
 <template>
-  <div class="carrusel d-flex justify-content-center align-items-start">
+  <div class="carrusel d-flex justify-content-start align-items-end">
     <div
       id="carouselExampleIndicators"
       class="carousel slide"
       data-bs-ride="true"
-      style="max-width: 1220px; margin-bottom: 100px"
     >
       <div class="carousel-indicators">
         <div class="carrusel-indicators-inside d-flex justify-content-center">
@@ -30,7 +29,7 @@
             <div
               class="sub-item d-flex justify-content-center align-items-center"
             >
-              <div style="width: 80%">
+              <div class="text" style="width: 80%">
                 <h1 class="title-item-carrusel">
                   {{ element.title }}
                 </h1>
@@ -80,62 +79,76 @@ export default {
 </script>
 <style scoped lang="scss">
 #carouselExampleIndicators {
-  border: solid 1px $color-border-carrusel;
-  border-radius: 20px;
-}
+  padding-left: 5%;
+  padding-right: 5%;
+  margin-bottom: 100px;
+  .carousel-inner {
+    border: solid 2px $color-border-carrusel;
+    border-radius: 20px;
+    .item {
+      .sub-item {
+        .title-item-image {
+          border-radius: 20px;
+          object-fit: cover;
+        }
+      }
+    }
+  }
+  .carousel-indicators {
+    margin-bottom: -2.3rem !important ;
+    .button-slider {
+      border-radius: 50%;
+      background: #4970b0;
+      border: solid 2px #fff;
+      width: 0.1mm !important;
+      height: 0.1mm !important;
+      padding: 7px;
+      opacity: 1;
+    }
+    .active {
+      border-radius: 50%;
+      border: solid 2px #fff;
+      background: black;
+      width: 0.1mm !important;
+      height: 0.1mm !important;
+      padding: 7px;
+    }
+  }
 
-.carousel-indicators {
-  margin-bottom: -2.3rem !important ;
-  .button-slider {
-    border-radius: 50%;
+  .carrusel-indicators-inside {
     background: #4970b0;
-    border: solid 2px #fff;
-    width: 0.1mm !important;
-    height: 0.1mm !important;
-    padding: 7px;
-    opacity: 1;
-  }
-  .active {
-    border-radius: 50%;
-    border: solid 2px #fff;
-    background: black;
-    width: 0.1mm !important;
-    height: 0.1mm !important;
-    padding: 7px;
+    padding: 6px;
+    padding-left: 10px;
+    padding-right: 10px;
+    gap: 17px;
+    border-radius: 20px;
+    opacity: 0.7;
   }
 }
-.carrusel-indicators-inside {
-  background: #4970b0;
-  padding: 6px;
-  padding-left: 10px;
-  padding-right: 10px;
-  gap: 17px;
-  border-radius: 20px;
-  opacity: 0.7;
-}
 
-.carousel-inner {
-  border: solid 1px #3d7eb9;
-  border-radius: 20px;
-}
-.title-item-carrusel {
-  font-family: 'Poppins';
-  /* font-weight: 700; */
-  font-weight: 900;
-  /* font-size: 55px; */
-  font-size: 3vw;
-  /* line-height: 50px; */
-  line-height: 3vw;
-  letter-spacing: 0.04em;
-}
-.title-item-image {
-  border-radius: 20px;
-}
-
-.carousel-inner {
-  .item {
-    display: flex;
-    flex-direction: column-reverse;
+@media (max-width: $movil-xl-size) {
+  .carousel-inner {
+    .item {
+      display: flex;
+      flex-direction: column-reverse;
+      .sub-item {
+        .text {
+          margin-top: 10px;
+          margin-bottom: 5px;
+          .title-item-carrusel {
+            font-family: 'Poppins';
+            font-weight: 700;
+            font-size: 4vw;
+            line-height: 4vw;
+            letter-spacing: 0.04em;
+          }
+          .text-item-carrusel {
+            font-size: 3vw;
+            line-height: 3vw;
+          }
+        }
+      }
+    }
   }
 }
 
@@ -144,9 +157,47 @@ export default {
     .item {
       display: flex;
       flex-direction: row;
+      .sub-item {
+        width: 50%;
+        .text {
+          .title-item-carrusel {
+            font-family: 'Poppins';
+            font-weight: 700;
+            font-size: 2.5vw;
+            line-height: 2.5vw;
+            letter-spacing: 0.04em;
+          }
+          .text-item-carrusel {
+            font-size: 1.7vw;
+            line-height: 2.5vw;
+          }
+        }
+      }
     }
-    .sub-item {
-      width: 50%;
+  }
+}
+
+@media (min-width: $desktop-size) {
+  .carousel-inner {
+    .item {
+      display: flex;
+      flex-direction: row;
+      .sub-item {
+        width: 50%;
+        .text {
+          .title-item-carrusel {
+            font-family: 'Poppins';
+            font-weight: 700;
+            font-size: 3.5vw;
+            line-height: 3.5vw;
+            letter-spacing: 0.04em;
+          }
+          .text-item-carrusel {
+            font-size: 1.7vw;
+            line-height: 2vw;
+          }
+        }
+      }
     }
   }
 }
